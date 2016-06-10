683b6ed02c31b48ac9b325b2389a1c5d5af9c735
Brian Hrolenok <brian.hrolenok@gmail.com>
2016-06-10 17:47:16 -0400

    Initial commit of templates from the wiki
    Notes:
    1) Not all assignments have directories. This may change if we use
       the same process for submitting manual assignments as autograded
    
    2) All *.pyc files have been removed
    
    3) util.py exists in multiple places in the repository. This will
       be refactored so that it exists in a single place so that we
       can push updates to a single file. This may involve telling
       students to use the "python -m modulename.mainfile" syntax.

---
+++ mc1_hw1/stdev_code.py
@@ -0,0 +1,16 @@
+import math as m
+
+# calculate the population standard deviation
+def stdev_p(data):
+    result = 2.0 # your code goes here
+    return result
+
+# calculate the sample standard deviation
+def stdev_s(data):
+    result = 1.9 # your code goes here
+    return result
+
+if __name__ == "__main__":
+    test = [2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]
+    print "the population stdev is", stdev_p(test)
+    print "the sample stdev is", stdev_s(test)
