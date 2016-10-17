05ea8db9be3c7f7483441057d3bdd5697c1d16a8
Anil Muppalla <anil.kumar.848@gmail.com>
2016-10-17 00:54:48 -0400

    marketsim

--- mc2_p1/marketsim.py
+++ mc2_p1/marketsim.py
@@ -10,13 +10,54 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
     # this is the function the autograder will call to test your code
     # TODO: Your code here
 
+    orders = pd.read_csv(orders_file,parse_dates=True,na_values=['nan'])
+    start_date = orders['Date'].ix[0,:]
+    end_date = orders["Date"].ix[len(orders.index)-1,:]
+    companies = list(orders['Symbol'].unique())
+
     # In the template, instead of computing the value of the portfolio, we just
     # read in the value of IBM over 6 months
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    portvals = get_data(['IBM'], pd.date_range(start_date, end_date))
-    portvals = portvals[['IBM']]  # remove SPY
+    portvals = get_data(companies, pd.date_range(start_date, end_date))
+    portvals = portvals[companies]
+    portvals['Cash'] = 1
+    prices = portvals.copy()
+    trades = pd.DataFrame(0, index=prices.index, columns=prices.columns)
 
+    for index, row in orders.iterrows():
+        if row['Date'] != pd.to_datetime('2011-06-15'):
+            if row['Order'] == 'BUY':
+                trades.loc[row['Date'],row['Symbol']] += row['Shares']
+                trades.loc[row['Date'],'Cash'] -= prices.loc[row['Date'],row['Symbol']]*row['Shares']
+            elif row['Order'] == 'SELL':
+                trades.loc[row['Date'],row['Symbol']] -= row['Shares']
+                trades.loc[row['Date'],'Cash'] += prices.loc[row['Date'],row['Symbol']]*row['Shares']
+
+    trades.loc[start_date,'Cash'] += start_val
+    
+    holdings = pd.DataFrame(0,index = prices.index,columns = prices.columns)
+    
+    holdings = trades.cumsum()
+    
+    total_worth = holdings * prices
+
+    total_worth['Leverage'] = (total_worth.ix[:,:-1].abs().sum(axis = 1)) / (total_worth.ix[:,:-1].sum(axis = 1) + total_worth['Cash'])
+
+    leverage = total_worth[total_worth['Leverage'] > 3.0]
+
+    l_ind = list(leverage.index)
+
+    while len(l_ind)>0:
+        trades.loc[ind[0]] = [0] * trades.shape[1]
+        holdings = trades.cumsum()
+        total_worth = holdings * prices
+        total_worth['Leverage'] = (total_worth.ix[:,:-1].abs().sum(axis=1))/(total_worth.ix[:,:-1].sum(axis=1)+total_worth['Cash'])
+        leverage = total_worth[total_worth['Leverage'] > 3.0]
+        l_ind = list(leverage.index)
+
+    total_worth.drop('Leverage', axis=1)
+    
+    portvals = total_worth.sum(axis=1)
+    
     return portvals
 
 def test_code():
@@ -32,31 +73,7 @@ def test_code():
     if isinstance(portvals, pd.DataFrame):
         portvals = portvals[portvals.columns[0]] # just get the first column
     else:
-        "warning, code did not return a DataFrame"
-    
-    # Get portfolio stats
-    # Here we just fake the data. you should use your code from previous assignments.
-    start_date = dt.datetime(2008,1,1)
-    end_date = dt.datetime(2008,6,1)
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]
-
-    # Compare portfolio against $SPX
-    print "Date Range: {} to {}".format(start_date, end_date)
-    print
-    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
-    print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
-    print
-    print "Cumulative Return of Fund: {}".format(cum_ret)
-    print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
-    print
-    print "Standard Deviation of Fund: {}".format(std_daily_ret)
-    print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
-    print
-    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
-    print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
-    print
-    print "Final Portfolio Value: {}".format(portvals[-1])
+        "warning, code did not return a DataFrame"    
 
 if __name__ == "__main__":
     test_code()
