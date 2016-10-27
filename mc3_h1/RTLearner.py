ec32f4333f769cb1ef9b9baab17c6e9258e88e91
Anil Muppalla <anil.kumar.848@gmail.com>
2016-10-26 20:24:05 -0400

    mc3 h1

--- mc3_h1/RTLearner.py
+++ mc3_h1/RTLearner.py
@@ -1,15 +1,13 @@
 """
-A FAKE Random Tree Learner.  (c) 2016 Tucker Balch
-This is just a linear regression learner implemented named RTLearner so
-it is used as a template or placeholder for use by testbest4.  You should
-replace this code with your own RTLearner.
+A simple wrapper for linear regression.  (c) 2015 Tucker Balch
 """
 
 import numpy as np
 
 class RTLearner(object):
 
-    def __init__(self, verbose = False, leaf_size = 1):
+    def __init__(self, leaf_size = 1, verbose = False):
+        self.leaf_size = leaf_size
         pass # move along, these aren't the drones you're looking for
 
     def addEvidence(self,dataX,dataY):
@@ -18,25 +16,54 @@ class RTLearner(object):
         @param dataX: X values of data to add
         @param dataY: the Y training values
         """
+        self.random_tree = self.build_random_tree(dataX,dataY)
 
-        # slap on 1s column so linear regression finds a constant term
-        newdataX = np.ones([dataX.shape[0],dataX.shape[1]+1])
-        newdataX[:,0:dataX.shape[1]]=dataX
+    def build_random_tree(self, dataX, dataY):
+        #factor, splitval, left, right
+        # if dataX.shape[0] == 0:
+        #     return np.array([[-1,-1,-1,-1]])
+        if dataX.shape[0] <= self.leaf_size:
+            return np.array([[-1, np.mean(dataY), -1, -1]])
+        if len(np.unique(dataY)) == 1: 
+            return np.array([[-1, dataY[0], -1, -1]])
 
-        # build and save the model
-        self.model_coefs, residuals, rank, s = np.linalg.lstsq(newdataX, dataY)
+        leftTreeSize = rightTreeSize = 0
+
+        while leftTreeSize == 0 or rightTreeSize == 0:
+            randomFeature = np.random.randint(0,dataX.shape[1])
+            randomFeatureRow1 = np.random.randint(0,dataX.shape[0])
+            randomFeatureRow2 = np.random.randint(0,dataX.shape[0])
+            splitVal = (dataX[randomFeatureRow1, randomFeature] + dataX[randomFeatureRow2,randomFeature])/float(2)
+            leftTreeSize = dataX[dataX[:,randomFeature]<=splitVal].shape[0]
+            rightTreeSize = dataX[dataX[:,randomFeature]>splitVal].shape[0]
         
+        leftTree = self.build_random_tree(dataX[dataX[:,randomFeature]<=splitVal], dataY[dataX[:,randomFeature]<=splitVal])
+            
+        rightTree = self.build_random_tree(dataX[dataX[:,randomFeature]>splitVal], dataY[dataX[:,randomFeature] > splitVal])
+
+        root = [randomFeature, splitVal, 1, leftTree.shape[0]+1]
+        
+        return np.vstack((root,leftTree,rightTree))
+
+    def search(self,point,index):
+        node = self.random_tree[index]
+        if node.item(0) == -1:
+            return node.item(1)
+        elif point[int(node.item(0))] <= node.item(1):
+            return self.search(point, int(node.item(2)) + index)
+        else:
+            return self.search(point, int(node.item(3)) + index)
+
     def query(self,points):
         """
         @summary: Estimate a set of test points given the model we built.
         @param points: should be a numpy array with each row corresponding to a specific query.
         @returns the estimated values according to the saved model.
         """
-        # get the linear result
-        ret_val = (self.model_coefs[:-1] * points).sum(axis = 1) + self.model_coefs[-1]
-        # add some random noise
-	ret_val = ret_val + 0.09 * np.random.normal(size = ret_val.shape[0])
-	return ret_val
+        predictY = np.empty(shape=(points.shape[0],))
+        for i in range(0,len(points)):
+            predictY[i] = self.search(points[i],0)
+        return predictY
 
 if __name__=="__main__":
-    print "get me a shrubbery"
+    print "LULZ!"
--- mc3_h1/gen_data.py
+++ mc3_h1/gen_data.py
@@ -8,13 +8,31 @@ import math
 # this function should return a dataset (X and Y) that will work
 # better for linear regresstion than random trees
 def best4LinReg():
-    X = np.random.normal(size = (100, 4)) 
-    Y = 0.1 * X[:,0] + 0.5 * np.sin(X[:,1]) + 0.1 * np.random.normal(size = 100)
+    X = np.random.normal(size = (250, 4)) 
+    Y = 0.45 * X[:,0] + 0.4 * X[:,1] - 0.2 * X[:,2] + 0.6 * X[:,3] + 0.5
     return X, Y
 
 def best4RT():
-    X = np.random.normal(size = (50, 2)) 
-    Y = 0.8 * X[:,0] + 5.0 * X[:,1] 
+    X = np.random.choice(1, size = (200, 3)) 
+    Y = np.zeros(shape=(200,))
+    for i in xrange(X.shape[0]):
+        x = X[i, :]
+        if x[0]>0 and x[1]>0 and x[2]>0:
+            Y[i] = 1
+        elif x[0]>0 and x[1]>0 and x[2]<=0:
+            Y[i] = 2
+        elif x[0]>0 and x[1]<=0 and x[2]>0:
+            Y[i] = 2
+        elif x[0]>0 and x[1]<=0 and x[2]<=0:
+            Y[i] = 1
+        elif x[0]<=0 and x[1]>0 and x[2]<=0:
+            Y[i] = 1
+        elif x[0]<=0 and x[1]<=0 and x[2]>0:
+            Y[i] = 2
+        elif x[0]<=0 and x[1]<=0 and x[2]<=0:
+            Y[i] = 1
+        elif x[0]<=0 and x[1]>0 and x[2]>0:
+            Y[i] = 2
     return X, Y
 
 if __name__=="__main__":
