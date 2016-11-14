0bdc63cc5763b2de4fff85f539bd00844e97f415
Anil Muppalla <anil.kumar.848@gmail.com>
2016-11-13 22:42:28 -0500

    qlearner

--- mc3_p2/QLearner.py
+++ mc3_p2/QLearner.py
@@ -18,9 +18,20 @@ class QLearner(object):
         verbose = False):
 
         self.verbose = verbose
+        self.num_states = num_states
         self.num_actions = num_actions
+        self.alpha = alpha
+        self.gamma = gamma
+        self.rar = rar
+        self.radr = radr
+        self.dyna = dyna
         self.s = 0
         self.a = 0
+        self.q = np.random.uniform(low=-1.0,high=1.0,size=(num_states,num_actions))
+        self.T = np.zeros((num_states, num_actions, num_states))
+        self.Tc = np.full((num_states, num_actions, num_states), 0.000001)
+        self.R = np.zeros((num_states, num_actions))
+        self.alphaR = 0.9
 
     def querysetstate(self, s):
         """
@@ -29,7 +40,12 @@ class QLearner(object):
         @returns: The selected action
         """
         self.s = s
-        action = rand.randint(0, self.num_actions-1)
+        prob = np.random.rand()
+        if prob < self.rar:
+            action = rand.randint(0, self.num_actions-1)
+        else:
+            action = self.q[s,:].argmax()
+        self.a = action
         if self.verbose: print "s =", s,"a =",action
         return action
 
@@ -40,7 +56,31 @@ class QLearner(object):
         @param r: The ne state
         @returns: The selected action
         """
-        action = rand.randint(0, self.num_actions-1)
+        prob = np.random.rand()
+        if prob<self.rar:
+            action = rand.randint(0, self.num_actions-1)
+        else:
+            action = self.q[s_prime,:].argmax()
+        
+        self.rar = self.rar * self.radr
+        
+        self.q[self.s,self.a] = (1-self.alpha) * self.q[self.s,self.a] + self.alpha * (r + self.gamma * self.q[s_prime, :].max())
+
+        if self.dyna > 0:
+            self.Tc[self.s,self.a,s_prime] += 1
+            self.T[self.s,self.a,s_prime] = self.Tc[self.s,self.a,s_prime]/self.Tc[self.s,self.a,:].sum()
+            self.R[self.s,self.a] = (1 - self.alpha)*(self.R[self.s,self.a]) + (self.alpha*r)
+        
+            for i in xrange(self.dyna):
+                dynastate = rand.randint(0,self.num_states-1)
+                dynaction = rand.randint(0,self.num_actions-1)
+                s_new = self.T[dynastate,dynaction,:].argmax()
+                dynaR = self.R[dynastate,dynaction]
+                self.q[dynastate,dynaction] = (1 - self.alphaR) * (self.q[dynastate,dynaction]) + (self.alphaR)*(dynaR + self.gamma*self.q[s_new, :].max())
+
+        self.s = s_prime
+        self.a = action
+        
         if self.verbose: print "s =", s_prime,"a =",action,"r =",r
         return action
 
