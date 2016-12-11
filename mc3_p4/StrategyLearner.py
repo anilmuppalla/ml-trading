89ead0bcc785f3a39a626e7fa8ad44df0478e963
Anil Muppalla <anil.kumar.848@gmail.com>
2016-12-11 02:36:34 -0500

    test

--- mc3_p4/StrategyLearner.py
+++ mc3_p4/StrategyLearner.py
@@ -5,194 +5,242 @@ Template for implementing StrategyLearner  (c) 2016 Tucker Balch
 import datetime as dt
 import QLearner as ql
 import pandas as pd
-import numpy as np
 import util as ut
+import numpy as np
 
 class StrategyLearner(object):
 
     # constructor
     def __init__(self, verbose = False):
         self.verbose = verbose
-        self.LONG = 0
-        self.SHORT = 1
-        self.NOTHING = 2
 
-    def indicators(self, prices):
-        prices = prices.fillna(method='ffill')
-        prices = prices.fillna(method='bfill')
-        prices = prices/prices.ix[0,:]
-        lookback = 5
-        
-        sma = pd.rolling_mean(prices,window=lookback,min_periods=lookback)
-        sma = sma.fillna(method ='bfill')
-        sma_avg = (prices/sma) * 100.0
-
-        rolling_std = pd.rolling_std(prices,window=lookback,min_periods=lookback)
-        top_band = sma + (2 * rolling_std)
-        bottom_band = sma - (2 * rolling_std)
-        bbp = (prices - bottom_band) / (top_band - bottom_band)
-        bbp = bbp.fillna(method ='bfill')
-        
-        momentum = prices.copy()
-        for symbol in prices.columns:
-            print symbol
-            momentum[symbol] = 0
-            for i in range(lookback, len(prices)):
-                momentum[symbol][i] = (prices[symbol][i] / prices[symbol][i - lookback] - 1) * 100
-
-        #TODO: Discretize indicators 
-        sma_avg = np.floor(((sma_avg - sma_avg.min()) / (sma_avg.max() - sma_avg.min())) * 9)
-        
-        # print sma_avg
-
-        bbp = np.floor(((bbp - bbp.min()) / (bbp.max() - bbp.min())) * 9)
-        
-        momentum = np.floor(((momentum - momentum.min()) / (momentum.max() - momentum.min())) * 9)
-        
-        indicator_list = pd.concat([sma_avg, momentum], axis=1)
-        indicator_list.columns = ['SMA', 'Momentum']
-        indicator_list = indicator_list.fillna(method='bfill')
-        
-        # print indicator_list
-        return indicator_list
-
+    # this method should create a QLearner, and train it for trading
     def addEvidence(self, symbol = "IBM", \
-        sd=dt.datetime(2006,1,1), \
-        ed=dt.datetime(2009,12,31), \
-        sv = 100000): 
+        sd=dt.datetime(2008,1,1), \
+        ed=dt.datetime(2009,1,1), \
+        sv = 10000): 
 
-        self.learner = ql.QLearner()
-      
+        self.learner = ql.QLearner(num_states=1000,\
+        num_actions = 3, \
+        alpha = 0.2, \
+        gamma = 0.9, \
+        rar = 0.98, \
+        radr = 0.999, \
+        dyna = 0, \
+        verbose=False) #initialize the learner
+
+        lookback = 14
         syms=[symbol]
-        dates = pd.date_range(sd, ed)
+        dates = pd.date_range(sd - dt.timedelta(lookback*3), ed)  # get extra data for lookback period
         prices_all = ut.get_data(syms, dates)  # automatically adds SPY
-        prices = prices_all[syms]  # only portfolio symbols
+        prices = prices_all[symbol]  # only portfolio symbol
+        if self.verbose: print prices
 
-        converged = False
-        holding = 0
-        iters= 0
-        portval = 100000
-        prev_portval = 100000
-        prev_holding = 0
-        x = self.indicators(prices)
+        # Price/SMA
+        sma = pd.rolling_mean(prices, window=lookback)
+        psmaratio = prices / sma
+        psmaratio = psmaratio[sd:]
+        # print psmaratio
+        # Momentum
+        momentum = prices.copy()
+        momentum[:] = 0
+        momentum[lookback:] = (prices[lookback:] / prices[:-lookback].values - 1) * 100
+        momentum = momentum[sd:]
+        # Ulcer Index
+        pdrawdown = prices.copy()
+        pdrawdown[:] = 0
+        maximum = pd.rolling_max(prices, window=lookback)
+        pdrawdown[1:] = (prices[1:] - maximum[:-1].values) / maximum[:-1].values * 100
+        sq = pdrawdown * pdrawdown
+        sqa = pd.rolling_sum(sq, window=lookback) / lookback
+        ulceridx = prices.copy()
+        ulceridx[:] = 0
+        ulceridx[1:] = np.sqrt(sqa[:-1].values)
+        ulceridx = ulceridx[sd:]
 
-        while not converged and iters<=100:
+        prices = prices[sd:]
 
-            query_state = x.ix[0,0]*10 + x.ix[0,1] #* 100 + x.ix[0,2]
-            
-            action = self.learner.querysetstate(query_state)
+        steps = 10
+        stepsize = len(prices) / steps
+        print stepsize
+        self.thresholdPsmaratio = range(0, steps-1)
+        data = psmaratio.sort_values()
+        print data
+        for i in range(0, steps-1):
+            self.thresholdPsmaratio[i] = data[int(i * stepsize)]
+        print self.thresholdPsmaratio
+        print psmaratio
+        discPsmaratio = np.searchsorted(self.thresholdPsmaratio, psmaratio, side='left')
+        print discPsmaratio
 
-            portval = sv
-            if action == self.LONG:
-                holding = 500
-                portval -= holding*prices.ix[0,0]
+        self.thresholdMomentum = range(0, steps-1)
+        data = momentum.sort_values()
+        for i in range(0, steps-1):
+            self.thresholdMomentum[i] = data[int(i * stepsize)]
+        discMomentum = np.searchsorted(self.thresholdMomentum, momentum, side='left')
 
-            elif action == self.SHORT:
-                holding = -500
-                portval -= holding*prices.ix[0,0]
-            
-            for i in xrange(1, prices.shape[0]):
-                # calculate daily portfolio return based on whether youre holding or not
-                
-                if holding == 0 or action == self.NOTHING:
-                    reward = 0
-                elif action == self.LONG:
-                    reward = (prices.ix[i,0] - prices.ix[i-1,0])*500
-                else:
-                    reward = -(prices.ix[i,0] - prices.ix[i-1,0])*500 # %age change in portfolio return daily, based on holdings acc. to policy
+        self.thresholdUlceridx = range(0, steps-1)
+        data = ulceridx.sort_values()
+        for i in range(0, steps-1):
+            self.thresholdUlceridx[i] = data[int(i * stepsize)]
+        discUlceridx = np.searchsorted(self.thresholdUlceridx, ulceridx, side='left')
 
-                query_state = x.ix[i,0]*10 + x.ix[i,1] #*10 + x.ix[i,2]
-                
-                action = self.learner.query(query_state,reward)
-                
-                if action == self.LONG:
-                    if prev_holding == 500:
-                        pass #action = self.NOTHING
-                    else:
-                        holding = 500
-                        portval -= (holding - prev_holding) * prices.ix[i,0]
-                elif action == self.SHORT:
-                    
-                    if prev_holding == -500:
-                        pass #action = self.NOTHING
-                    else:
-                        holding = -500
-                        portval -= (holding - prev_holding) * prices.ix[i,0]
-                else:
-                    holding = 0
-                    portval -= (holding - prev_holding) * prices.ix[i,0]
-                
-                #print action, prev_portval, prev_holding, portval, holding
-                prev_holding = holding
+        count = 0
+        totrewardlast = 0
+        totreward = 1
+        while ((totrewardlast != totreward) & (count < 1000)) | (count < 50):
+            totrewardlast = totreward
+            holding = 0
+            trade = 0
+            value = 0
+            cash = sv
+            portval = 0
+            pos = 0  # pos = -1: short, pos = 0: nothing, pos = 1: long
+            x = discretize(discPsmaratio[0], discMomentum[0], discUlceridx[0])
+            action = self.learner.querysetstate(x)
+            for i in range(1,len(prices)):
+                if action == 0:  # Be Short
+                    if pos == -1:
+                        trade = 0
+                    elif pos == 0:
+                        trade = -500
+                    elif pos == 1:
+                        trade = -1000
+                    pos = -1
+                elif action == 1:  # Be Nothing
+                    if pos == -1:
+                        trade = 500
+                    elif pos == 0:
+                        trade = 0
+                    elif pos == 1:
+                        trade = -500
+                    pos = 0
+                elif action == 2:  # Be Long
+                    if pos == -1:
+                        trade = 1000
+                    elif pos == 0:
+                        trade = 500
+                    elif pos == 1:
+                        trade = 0
+                    pos = 1
+                if i < (len(prices) - 1):
+                    holding = holding + trade
+                    value = prices[i]*holding
+                    cash = cash - prices[i]*trade
+                    portvalcurrent = value + cash
+                    value = prices[i+1]*holding
+                    portval = value + cash
+                    r = portval/portvalcurrent - 1
 
-            iters += 1
-            
-            # print prev_portval, portval
-            if  portval == prev_portval:
-                    converged = True
-            prev_portval = portval
+                    x = discretize(discPsmaratio[i], discMomentum[i], discUlceridx[i])
+                    action = self.learner.query(x, r)
+            totreward = portval  # calculate portfolio value
+            count += 1
+
+        # print count
+        # print totreward
+
 
     # this method should use the existing policy and test it against new data
     def testPolicy(self, symbol = "IBM", \
-        sd=dt.datetime(2010,1,1), \
-        ed=dt.datetime(2010,12,31), \
-        sv = 100000):
+        sd=dt.datetime(2009,1,1), \
+        ed=dt.datetime(2010,1,1), \
+        sv = 10000):
 
-        # here we build a fake set of trades
-        # your code should return the same sort of data
-        dates = pd.date_range(sd, ed)
-        prices_all = ut.get_data([symbol], dates)  # automatically adds SPY
-        trades = prices_all[[symbol,]].copy()
-        prices = prices_all[[symbol]].copy() # only portfolio symbols
+        lookback = 14
+        syms = [symbol]
+        dates = pd.date_range(sd - dt.timedelta(lookback * 3), ed)
+        prices_all = ut.get_data(syms, dates)  # automatically adds SPY
+        prices = prices_all[symbol]  # only portfolio symbols
+        if self.verbose: print prices
 
-        # print prices
+        # Price/SMA
+        sma = pd.rolling_mean(prices, window=lookback)
+        psmaratio = prices / sma
+        psmaratio = psmaratio[sd:]
+        # Momentum
+        momentum = prices.copy()
+        momentum[:] = 0
+        momentum[lookback:] = (prices[lookback:] / prices[:-lookback].values - 1) * 100
+        momentum = momentum[sd:]
+        # Ulcer Index
+        pdrawdown = prices.copy()
+        pdrawdown[:] = 0
+        maximum = pd.rolling_max(prices, window=lookback)
+        pdrawdown[1:] = (prices[1:] - maximum[:-1].values) / maximum[:-1].values * 100
+        sq = pdrawdown * pdrawdown
+        sqa = pd.rolling_sum(sq, window=lookback) / lookback
+        ulceridx = prices.copy()
+        ulceridx[:] = 0
+        ulceridx[1:] = np.sqrt(sqa[:-1].values)
+        ulceridx = ulceridx[sd:]
 
-        if self.verbose: print type(trades) # it better be a DataFrame!
-        if self.verbose: print trades
-        if self.verbose: print prices_all
+        prices = prices[sd:]
 
-        x = self.indicators(prices)
+        discPsmaratio = np.searchsorted(self.thresholdPsmaratio, psmaratio, side='left')
+        discMomentum = np.searchsorted(self.thresholdMomentum, momentum, side='left')
+        discUlceridx = np.searchsorted(self.thresholdUlceridx, ulceridx, side='left')
+
+        df_trades = prices.copy()
+        df_trades[:] = 0
 
         holding = 0
-        prev_holding = 0
-        
-        la = {0 : "BUY", 1 : "SELL", 2: "NOTHING" }
+        trade = 0
+        value = 0
+        cash = sv
+        pos = 0  # pos = -1: short, pos = 0: nothing, pos = 1: long
+        x = discretize(discPsmaratio[0], discMomentum[0], discUlceridx[0])
+        action = self.learner.querysetstate(x)
+        for i in range(1, len(prices)):
+            if action == 0:  # Be Short
+                if pos == -1:
+                    trade = 0
+                elif pos == 0:
+                    trade = -500
+                elif pos == 1:
+                    trade = -1000
+                pos = -1
+            elif action == 1:  # Be Nothing
+                if pos == -1:
+                    trade = 500
+                elif pos == 0:
+                    trade = 0
+                elif pos == 1:
+                    trade = -500
+                pos = 0
+            elif action == 2:  # Be Long
+                if pos == -1:
+                    trade = 1000
+                elif pos == 0:
+                    trade = 500
+                elif pos == 1:
+                    trade = 0
+                pos = 1
+            df_trades[i] = trade
+            if i < (len(prices) - 1):
+                holding = holding + trade
+                # print "holding: ", holding
+                value = prices[i] * holding
+                # print "value: ", value
+                cash = cash - prices[i] * trade
+                # print "cash: ", cash
+                portvalcurrent = value + cash
+                value = prices[i + 1] * holding
+                portval = value + cash
+                # print "portval: ", portval
+                # print "portvalcurrent: ", portvalcurrent
+                
+                r = portval / portvalcurrent - 1
+
+                x = discretize(discPsmaratio[i], discMomentum[i], discUlceridx[i])
+                action = self.learner.query(x, r)
+
+        # print portval
+        return df_trades.to_frame()
 
 
-        # f = open("orders.csv", "w")
-        # f.write("Date,Symbol,Order,Shares\n")
-    # 
-    # for order in df_orders:
-    #     f.write(order)
-    # f.close()
-        for i in xrange(0,prices.shape[0]):
-            # print i
-            query_state = x.ix[i,0]*10 + x.ix[i,1] #*10 + x.ix[i,2]
-            # print "qs:", query_state
-            action = self.learner.querysetstate(query_state)
-            if action == self.LONG:
-                if prev_holding == 500:
-                    pass #action = self.NOTHING
-                else:
-                    holding = 500
-            elif action == self.SHORT:
-                if prev_holding == -500:
-                    pass#action = self.NOTHING
-                else:
-                    holding = -500
-            else:
-                holding = 0
-            
-            trades.ix[i,0] = (holding - prev_holding)
-            prev_holding = holding 
-            
-            # print action
-            # print ",".join([str(prices.index[i].date()),str(symbol),str(la[action]),str(abs(holding))])
-            # f.write(",".join([str(prices.index[i].date()),str(symbol),str(la[action]),str(abs(holding))])+"\n")
+def discretize(psma, mom, ulc):
+    return psma*100 + mom*10 + ulc
 
-        # f.close()
-        print trades
-        return trades
-
-if __name__=="__main__":
+if __name__ == "__main__":
     print "One does not simply think up a strategy"
