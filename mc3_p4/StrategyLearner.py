14fc12e26bfccd18db18ee009a20d1a966175459
Anil Muppalla <anil.kumar.848@gmail.com>
2016-12-11 12:57:43 -0500

    test

--- mc3_p4/StrategyLearner.py
+++ mc3_p4/StrategyLearner.py
@@ -17,8 +17,6 @@ class StrategyLearner(object):
     def indicators(self, prices, lookback):
         sma = pd.rolling_mean(prices, window=lookback)
         psmaratio = prices / sma
-        # print psmaratio
-        # Momentum
         momentum = prices.copy()
         momentum[:] = 0
         momentum[lookback:] = (prices[lookback:] / prices[:-lookback].values - 1) * 100
@@ -28,17 +26,18 @@ class StrategyLearner(object):
    
         steps = 10
         stepsize = len(prices) / steps
-        self.thresholdPsmaratio = range(0, steps-1)
+        
+        self.psma_bins = range(0, steps-1)
+        self.momentum_bins = range(0, steps-1)
+        
         data = psmaratio.sort_values()
         for i in range(0, steps-1):
-            self.thresholdPsmaratio[i] = data[int(i * stepsize)]
+            self.psma_bins[i] = data[int(i * stepsize)]
              
-        self.thresholdMomentum = range(0, steps-1)
         data = momentum.sort_values()
         for i in range(0, steps-1):
-            self.thresholdMomentum[i] = data[int(i * stepsize)]
+            self.momentum_bins[i] = data[int(i * stepsize)]
         
-    # this method should create a QLearner, and train it for trading
     def addEvidence(self, symbol = "IBM", \
         sd=dt.datetime(2008,1,1), \
         ed=dt.datetime(2009,1,1), \
@@ -61,7 +60,7 @@ class StrategyLearner(object):
         prices = prices_all[symbol]  # only portfolio symbol
         
         if self.verbose: print prices
-        
+
         psmaratio, momentum = self.indicators(prices, lookback)
         
         psmaratio = psmaratio[sd:]
@@ -70,13 +69,14 @@ class StrategyLearner(object):
 
         self.discindicators(prices, psmaratio, momentum)
 
-        discPsmaratio = np.searchsorted(self.thresholdPsmaratio, psmaratio, side='left')
+        psma_bin = np.searchsorted(self.psma_bins, psmaratio, side='left')
         
-        discMomentum = np.searchsorted(self.thresholdMomentum, momentum, side='left')
+        momentum_bin = np.searchsorted(self.momentum_bins, momentum, side='left')
 
         count = 0
         totrewardlast = 0
         totreward = 1
+        
         while ((totrewardlast != totreward) & (count < 1000)) | (count < 50):
             totrewardlast = totreward
             holding = 0
@@ -85,7 +85,7 @@ class StrategyLearner(object):
             cash = sv
             portval = 0
             pos = 0  # pos = -1: short, pos = 0: nothing, pos = 1: long
-            x = discretize(discPsmaratio[0], discMomentum[0])
+            x = psma_bin[0] * 10 + momentum_bin[0]
             action = self.learner.querysetstate(x)
             for i in range(1,len(prices)):
                 if action == 0:  # Be Short
@@ -114,21 +114,16 @@ class StrategyLearner(object):
                     pos = 1
                 if i < (len(prices) - 1):
                     holding = holding + trade
-                    value = prices[i]*holding
-                    cash = cash - prices[i]*trade
+                    value = prices[i] * holding
+                    cash = cash - prices[i] * trade
                     portvalcurrent = value + cash
-                    value = prices[i+1]*holding
+                    value = prices[i+1] * holding
                     portval = value + cash
-                    r = portval/portvalcurrent - 1
-
-                    x = discretize(discPsmaratio[i], discMomentum[i])
+                    r = portval / portvalcurrent - 1
+                    x = psma_bin[0] * 10 + momentum_bin[0]
                     action = self.learner.query(x, r)
             totreward = portval  # calculate portfolio value
             count += 1
-
-        # print count
-        # print totreward
-
 
     # this method should use the existing policy and test it against new data
     def testPolicy(self, symbol = "IBM", \
@@ -149,8 +144,8 @@ class StrategyLearner(object):
         momentum = momentum[sd:]
         prices = prices[sd:]
         
-        discPsmaratio = np.searchsorted(self.thresholdPsmaratio, psmaratio, side='left')
-        discMomentum = np.searchsorted(self.thresholdMomentum, momentum, side='left')
+        psma_bin = np.searchsorted(self.psma_bins, psmaratio, side='left')
+        momentum_bin = np.searchsorted(self.momentum_bins, momentum, side='left')
 
         df_trades = prices.copy()
         df_trades[:] = 0
@@ -160,7 +155,7 @@ class StrategyLearner(object):
         value = 0
         cash = sv
         pos = 0  # pos = -1: short, pos = 0: nothing, pos = 1: long
-        x = discretize(discPsmaratio[0], discMomentum[0])
+        x = psma_bin[0] * 10 + momentum_bin[0]
         action = self.learner.querysetstate(x)
         for i in range(1, len(prices)):
             if action == 0:  # Be Short
@@ -203,15 +198,11 @@ class StrategyLearner(object):
                 
                 r = portval / portvalcurrent - 1
 
-                x = discretize(discPsmaratio[i], discMomentum[i])
+                x = psma_bin[0] * 10 + momentum_bin[0]
                 action = self.learner.query(x, r)
 
         # print portval
         return df_trades.to_frame()
-
-
-def discretize(psma, mom):
-    return psma*10 + mom
 
 if __name__ == "__main__":
     print "One does not simply think up a strategy"
